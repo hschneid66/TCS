77ed6cd9627931476e08c94acbdb6f6ef82aad82
hschneid66 <holger.schneider@clearml.ai>
2026-06-08 16:09:12 +0100

    Update finetune.sh

--- finetune.sh
+++ finetune.sh
@@ -3,5 +3,5 @@
 pip install transformers peft accelerate datasets sentencepiece torch
 python finetune.py --dataset-id "dcba845f3369456a8d8befce12f55f80" --version-id "cfba2dff7cbe4d6fbabfdfc84c3c3699" \
     --model Qwen/Qwen2.5-1.5B-Instruct \
-    --batch-size 1000 --num-workers 16 \
+    --max-steps 5000 \
     --save-model
