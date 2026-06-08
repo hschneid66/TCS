03d48fa47b283dd410a65dd8e2ea65ae67f7562f
hschneid66 <holger.schneider@clearml.ai>
2026-06-08 09:20:40 +0100

    Create finetune.sh

---
+++ finetune.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+pip install transformers peft accelerate datasets sentencepiece torch
+python finetune.py --dataset-id "dcba845f3369456a8d8befce12f55f80" --version-id "cfba2dff7cbe4d6fbabfdfc84c3c3699" --max-steps 10000 --max-epochs 10000
