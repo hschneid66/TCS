e6cff525acd9fd5dbae4e17c471e8604dc10f46a
hschneid66 <holger.schneider@clearml.ai>
2026-06-08 14:06:59 +0100

    Update finetune.sh

--- finetune.sh
+++ finetune.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pip install transformers peft accelerate datasets sentencepiece torch
-python finetune.py --dataset-id "dcba845f3369456a8d8befce12f55f80" --version-id "cfba2dff7cbe4d6fbabfdfc84c3c3699" --max-steps 50000 --max-epochs 50000 \
-    --batch-size 1000 \
-    --num-workers 100 \
+python finetune.py --dataset-id "dcba845f3369456a8d8befce12f55f80" --version-id "cfba2dff7cbe4d6fbabfdfc84c3c3699" --max-steps 10000 --max-epochs 10000 \
+    --batch-size 100 \
+    --num-workers 32 \
     --lora-r 32 --lora-alpha 64
